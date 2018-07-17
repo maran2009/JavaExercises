6c5f599a0cfc84a752bc6010c74c77bcb0dbe683
Your Name <ilamaran.Murugesampillai@academytrainee.com>
2018-07-17 09:59:35 +0100

    Java project

---
+++ QAJavaProject/src/qajavaproject/QAJavaProject.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qajavaproject;
+
+/**
+ *
+ * @author Maran
+ */
+public class QAJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+       /* System.out.println("Hello world!"); // Q1 Hello world
+        
+        String var1 = "Hello";
+        String var2 = "World!";
+        
+        System.out.println(var1 + " " + var2); // Q2 Assignment
+        
+        String output = "";
+        
+        System.out.println(helloClass.hello(output));
+        
+    
+        int num[] = {1,2,3,5,6,7,4,9,8,10};
+        
+        for (int i =0; i<num.length; i++){ 
+        System.out.println(helloClass.sum(num[i], num[3], true));
+        */
+        
+        for (int j =1; j<=10; j++){
+            
+            System.out.print (j +" ,");
+           
+            
+             
+             }
+        System.out.println(" ");
+        
+        for (int k =0; k<=10; k++){
+            
+            System.out.print(k*10 +" ,");
+        }
+        }
+}
+        
+        
+    
+    
+   
+    
+
---
+++ QAJavaProject/src/qajavaproject/helloClass.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qajavaproject;
+
+/**
+ *
+ * @author Maran
+ */
+public class helloClass {
+    
+     public static String hello(String Hello){
+    
+   return Hello +" Hello World !!";
+    }
+
+public static int sum (int a, int b, boolean  R){
+   
+   
+    
+    int Total;
+    if (R ){
+        
+        if (a ==0){
+        return b;}
+        if (b == 0){
+        return a;}
+     
+}
+   else {
+        
+Total = a+b;
+
+return Total;
+
+    }
+
+return 0;
+}}
+
+
+
+
+
